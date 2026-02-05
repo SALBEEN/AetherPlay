4c8a15f4bd0504dcc6dae848c7d218f028cf0eca
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-05 21:37:05 +0545

    Added middleware and some utilities programs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // require("dotenv").config({ path: "./env" });
 
 import dotenv from "dotenv";
-import express from "express";
+// import express from "express";
 import connectDB from "./DB/index.js";
 
 dotenv.config({ path: "./env" });
 
-const app = express();
+// const app = express();
+
 /*
 (async () => {
   try {
@@ -24,7 +25,21 @@ const app = express();
 })();
 */
 
-connectDB();
+import { app } from "./app.js";
+
+app.get("/salben", () => {
+  console.log("Its Aether play Backend server");
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is running in : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Data base connection Failed!!!!!!");
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`App listening on ${process.env.PORT}`);
