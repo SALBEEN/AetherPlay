a35050dd24cac5eac243999f6b370fb700bcd982
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-08 21:11:47 +0545

    Added utils for file upload in cloudinary

---
+++ src/Utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+//configuring cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_API_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// async method as an utils for multiple usage
+const uploadFileCloudinary = async (localFileUrl) => {
+  try {
+    if (!localFileUrl) return null;
+
+    //upload file in cloudinary
+    const response = await cloudinary.v2.uploader.upload(localFileUrl, {
+      resource_type: "auto",
+    });
+
+    console.log("File uploaded to cloudinary successfully...!!!");
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFileUrl); //remove the locally uploaded file after upload operation failed...!!!
+    console.log("File uploading failed...!!!");
+  }
+};
+
+// cloudinary.v2.uploader
+//   .upload("dog.mp4", {
+//     resource_type: "video",
+//     public_id: "my_dog",
+//     overwrite: true,
+//     notification_url: "https://mysite.example.com/notify_endpoint",
+//   })
+//   .then((result) => console.log(result));
