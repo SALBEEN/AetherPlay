85fa86b5008068dbcf244b4e0397480592f68196
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-11 21:31:07 +0545

    Validate the data obtained from frontend

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,11 +1,39 @@
 import { asyncHandler } from "../Utils/AsyncHandler.js";
+import { ApiError } from "../Utils/ApiError.js";
 
 // controller for register
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "SERVER CHECK !!",
-  });
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+
+  const { username, email, fullName, password } = req.body;
+
+  if (fullName === "") {
+    throw new ApiError(409, "This field is required");
+  }
+  if (username === "") {
+    throw new ApiError(409, "This field is required");
+  }
+  if (email === "") {
+    throw new ApiError(409, "This field is required");
+  }
+  if (password.trim().length > 7 || password.trim() === "") {
+    throw new ApiError(409, "This field is required");
+  }
+
+  if (
+    [username, email, fullName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(409, "All the field is required");
+  }
 });
 
 // exporting methods
--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,11 +1,24 @@
 import { Router } from "express";
 import { registerUser } from "../Controllers/user.controller.js";
+import { upload } from "../Middlewares/multer.middleware.js";
 
 // creating a router service via express: method of express for routing
 const router = Router();
 
 // making a path that is directed to the actual controller
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
 
 export default router;
