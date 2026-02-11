924753b8943f5dae2333b4dd216dc44d40f60d9b
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-11 21:39:21 +0545

    Validate the data obtained from frontend

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,5 +1,7 @@
 import { asyncHandler } from "../Utils/AsyncHandler.js";
 import { ApiError } from "../Utils/ApiError.js";
+import { User } from "../Models/user.models.js";
+import { use } from "react";
 
 // controller for register
 
@@ -16,23 +18,39 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const { username, email, fullName, password } = req.body;
 
-  if (fullName === "") {
-    throw new ApiError(409, "This field is required");
-  }
-  if (username === "") {
-    throw new ApiError(409, "This field is required");
-  }
-  if (email === "") {
-    throw new ApiError(409, "This field is required");
-  }
-  if (password.trim().length > 7 || password.trim() === "") {
-    throw new ApiError(409, "This field is required");
-  }
+  // both the option of validation is used first one is for begineer and second is a pro way
+
+  // if (fullName === "") {
+  //   throw new ApiError(409, "This field is required");
+  // }
+  // if (username === "") {
+  //   throw new ApiError(409, "This field is required");
+  // }
+  // if (email === "") {
+  //   throw new ApiError(409, "This field is required");
+  // }
+  // if (password.trim().length > 7 || password.trim() === "") {
+  //   throw new ApiError(409, "This field is required");
+  // }
 
   if (
     [username, email, fullName, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(409, "All the field is required");
+  }
+
+  //check if username and email already exists
+
+  // const existedUser = User.findOne({username, email}); -> it is used when use have to validae fields individually
+
+  // to validate multiple field at one gooooo
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "Username or email already exists. Try another!");
   }
 });
 
