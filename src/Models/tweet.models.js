ed6180cc75ece6bcc654af6e486045956bb19440
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-24 22:08:20 +0545

    Added models

--- src/Models/tweet.models.js
+++ src/Models/tweet.models.js
@@ -1,7 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const tweetSchema = new Schema({}, { timestamps: true });
+const tweetSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 tweetSchema.plugin(mongooseAggregatePaginate);
 
