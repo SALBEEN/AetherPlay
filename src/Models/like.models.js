d7489c197dbfa3e8951a8bb224ca15c1963e3581
salbeen chapagain <wizsalbeen69@gmail.com>
2026-02-24 22:18:27 +0545

    Added models

--- src/Models/like.models.js
+++ src/Models/like.models.js
@@ -14,6 +14,10 @@ const LikeSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "User",
     },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
   },
   {
     timestamps: true,
