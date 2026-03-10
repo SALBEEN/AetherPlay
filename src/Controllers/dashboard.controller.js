33fee1add00adfcd277d886240759d379a8c2036
salbeen chapagain <wizsalbeen69@gmail.com>
2026-03-10 16:13:25 +0545

    Working on dashboard Controller

--- src/Controllers/dashboard.controller.js
+++ src/Controllers/dashboard.controller.js
@@ -12,6 +12,62 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+  const user = req.user._id;
+
+  if (!user) {
+    throw new ApiError("cannot get user id");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(user)) {
+    throw new ApiError("Invalid User ");
+  }
+
+  const channelVideo = await Video.aggregate([
+    {
+      $match: {
+        owner: mongoose.Types.ObjectId(user),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerInfo",
+      },
+    },
+    {
+      $unwind: {
+        path: "$ownerInfo",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        isPublished: 1,
+        owner: {
+          channelName: "$ownerInfo.fullName",
+          channelAvatar: "$ownerInfo.avatar",
+        },
+      },
+    },
+  ]);
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { channelVideos: channelVideo },
+        "Channel video fetched successfully",
+      ),
+    );
 });
 
 export { getChannelStats, getChannelVideos };
