5b47b2e04bd9c413071f9e2deb77ff987a6e646e
salbeen chapagain <wizsalbeen69@gmail.com>
2026-03-12 20:07:01 +0545

    Working on final debugging

--- src/app.js
+++ src/app.js
@@ -18,9 +18,17 @@ app.use(cookieParser());
 //importing a routing environment
 
 import userRouter from "./Routes/user.routes.js";
+import tweetRouter from "./Routes/tweet.routes.js";
+import commentRouter from "./Routes/comment.routes.js";
+import playlistRouter from "./Routes/playlist.routes.js";
+import videoRouter from "./Routes/video.routes.js";
 
 // if any url starts with users that is direct the floe te userRouter file
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/tweet", tweetRouter);
 
 export { app };
