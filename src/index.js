480c4387699d2fe21ee0917eae79bd61ae778933
salbeen chapagain <wizsalbeen69@gmail.com>
2026-04-24 12:42:26 +0545

    -

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ dotenv.config({
   path: "./.env",
 });
 
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     });
-  })  
+  })
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
   });
