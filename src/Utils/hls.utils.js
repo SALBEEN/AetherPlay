c4fda7f8616ffff7ae26896653661e178ba094c6
salbeen chapagain <wizsalbeen69@gmail.com>
2026-05-10 14:08:01 +0545

    fix: rename getHLSUrl to getVideoUrl and update URL format for video retrieval

--- src/Utils/hls.utils.js
+++ src/Utils/hls.utils.js
@@ -1,5 +1,5 @@
-const getHLSUrl = (publicId) => {
-  return `https://res.cloudinary.com/dphbhc0fp/video/upload/sp_auto,q_auto,f_auto/${publicId}.m3u8`;
+const getVideoUrl = (publicId) => {
+  return `https://res.cloudinary.com/dphbhc0fp/video/upload/q_auto,f_auto/${publicId}.mp4`;
 };
 
 export { getHLSUrl };
